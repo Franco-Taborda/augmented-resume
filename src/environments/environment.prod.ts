45328ba4d034e615c7a346cb8be64322eaf991f9
Franco <franco_x@outlook.com>
2022-03-09 14:58:06 -0300

    version update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: '0.0.7',
+  version: '0.0.8',
   url: 'https://dx50c2r2pohi7.cloudfront.net',
   emailUrl: 'https://formspree.io/f/mqknbnvv',
 };
