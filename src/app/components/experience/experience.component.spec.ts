0fda3d41f0923526892d142d495591488370bcb9
Franco <franco_x@outlook.com>
2022-03-17 17:09:34 -0300

    experience component

--- src/app/components/experience/experience.component.spec.ts
+++ src/app/components/experience/experience.component.spec.ts
@@ -1,24 +1,85 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
+import { ExperienceService } from 'services/experience.service';
+import { SharedModule } from 'shared/shared.module';
+import { EXPERIENCE_MOCK } from 'utils/mocks/experience.mock';
 
 import { ExperienceComponent } from './experience.component';
 
 describe('ExperienceComponent', () => {
   let component: ExperienceComponent;
   let fixture: ComponentFixture<ExperienceComponent>;
+  let debugElement: DebugElement;
+  let experienceService: ExperienceService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ExperienceComponent],
-    }).compileComponents();
-  });
+      imports: [SharedModule],
+      providers: [ExperienceService],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(ExperienceComponent);
+        component = fixture.componentInstance;
+        debugElement = fixture.debugElement;
+        experienceService = TestBed.inject(ExperienceService);
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ExperienceComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+        spyOn(experienceService, 'getExperience').and.returnValue(of(EXPERIENCE_MOCK));
+        fixture.detectChanges();
+      });
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show a title', () => {
+    const title = debugElement.query(By.css('[data-test="title"]'));
+    expect(title).toBeTruthy();
+  });
+
+  it('should show experience cards', () => {
+    const experienceCard = debugElement.queryAll(By.css('[data-test="experience-card"]'));
+    expect(experienceCard).toHaveSize(EXPERIENCE_MOCK.length);
+  });
+
+  it('should show a company name', () => {
+    const companyNameElem = debugElement.query(By.css('[data-test="company-name"]'));
+    expect(companyNameElem?.nativeElement.textContent).toEqual(EXPERIENCE_MOCK[0].companyName);
+  });
+
+  it('should show a company url', () => {
+    const firstElem = debugElement.query(By.css('[data-test="experience-card"]'));
+    const companyUrlElem = firstElem.query(By.css('[data-test="company-url"]'));
+    expect(companyUrlElem?.nativeElement.href).toEqual(EXPERIENCE_MOCK[0].companyUrl);
+  });
+
+  it('should show an working period', () => {
+    const firstElem = debugElement.query(By.css('[data-test="experience-card"]'));
+    const workingPeriodElem = firstElem.query(By.css('[data-test="working-period"]'));
+    expect(workingPeriodElem).toBeTruthy();
+  });
+
+  it('should show a client name', () => {
+    const firstElem = debugElement.query(By.css('[data-test="experience-card"]'));
+    const clientNameElem = firstElem.query(By.css('[data-test="client-name"]'));
+    expect(clientNameElem.nativeElement.textContent).toEqual(EXPERIENCE_MOCK[0].projects[0].clientName);
+  });
+
+  it('should show a project description', () => {
+    const firstElem = debugElement.query(By.css('[data-test="experience-card"]'));
+    const projectDescription = firstElem.query(By.css('[data-test="project-description"]'));
+    const descriptionItem = firstElem.queryAll(By.css('[data-test="description-item"]'));
+    expect(projectDescription).toBeTruthy();
+    expect(descriptionItem).toHaveSize(EXPERIENCE_MOCK[0].projects[0].jobDescriptionItems.length);
+  });
+
+  it('should show a list of used technologies', () => {
+    const firstElem = debugElement.query(By.css('[data-test="experience-card"]'));
+    const usedTechnologiesElemList = firstElem.queryAll(By.css('[data-test="used-technology"]'));
+    expect(usedTechnologiesElemList).toHaveSize(EXPERIENCE_MOCK[0].projects[0].usedTechnologies.length);
   });
 });
--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IExperience } from 'components/experience/models/experience.interface';
+import { ExperienceService } from 'services/experience.service';
+import { map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { SkillSVGs } from 'components/skills/enums/skills.enum';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss'],
 })
-export class ExperienceComponent {
-  constructor() {}
+export class ExperienceComponent implements OnInit {
+  experience$: Observable<IExperience[]>;
+
+  constructor(private experienceService: ExperienceService) {}
+
+  ngOnInit(): void {
+    this.experience$ = this.experienceService.getExperience().pipe(
+      map((experienceList) => {
+        experienceList.forEach((exp) => {
+          exp.startDate = new Date(exp.startDate);
+          exp.endDate = new Date(exp.endDate);
+          exp.projects.forEach((project) => {
+            project.usedTechnologies.forEach(
+              (technology) => (technology.imgSrc = `${environment.url}/assets/images/${SkillSVGs[technology.name]}`),
+            );
+          });
+        });
+        return experienceList;
+      }),
+    );
+  }
 }
